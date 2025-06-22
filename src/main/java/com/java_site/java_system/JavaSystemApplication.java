bb5104583d58ec1abaa6f9ed19cda75fdd6968b5
Elisheva Tzemach <elisheva2005z@gmail.com>
2025-06-22 14:17:11 +0300

    First real commit

---
+++ src/main/java/com/java_site/java_system/JavaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.java_site.java_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_site/java_system/JavaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_site.java_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
